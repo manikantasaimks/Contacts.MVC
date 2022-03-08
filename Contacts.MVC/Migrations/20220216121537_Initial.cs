ba77bdcc8f5cc0552436eca71de04465ac7287c0
Manikanta Samaleti (Quadrant Resource) <manikanta.samaleti@quadrantresource.com>
2022-03-08 13:04:17 +0530

    Add project files.

---
+++ Contacts.MVC/Migrations/20220216121537_Initial.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Contacts.MVC.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contact",
+                columns: table => new
+                {
+                    EmployeeID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    EmployeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmployeeAge = table.Column<int>(type: "int", nullable: false),
+                    EmployeeType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    EmployeeAddress = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contact", x => x.EmployeeID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contact");
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Models
+{
+    public class Contact
+    {
+        [Key]
+        public int EmployeeID { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public int EmployeeAge { get; set; }
+        public string EmployeeType { get; set; }
+        public string EmployeeAddress { get; set; }
+    }
+}
